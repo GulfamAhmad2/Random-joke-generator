3ab1d08f1e31250f3d084451d472275b50f6026c
gulfam <gulfam@gmai.com>
2023-03-04 10:51:34 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
 function App() {
+  const [data, setData] = useState({});
+
+  const joke = () => {
+    fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single")
+      .then((res) => res.json())
+      .then((items) => {
+        setData(items);
+      });
+  };
+
+  useEffect(() => {
+    joke();
+  }, []);
+
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h2>Random Joke</h2>
+        <p>"{data.joke}"</p>
+        <button onClick={joke}>Get Joke</button>
+      </div>
+    </>
   );
 }
 
